65e0ec2f97371f8381d893fb7d4ac409bc696fba
tonywyb <1500012946@pku.edu.cn>
2019-09-17 02:49:35 -0400

    update .gitignore~

--- couchstuff/t1.py
+++
@@ -1,6 +0,0 @@
-import couchdb
-
-svr = couchdb.Server("http://dbuser:dbuserdbuser@localhost:5984/")
-print("Server = ", svr)
-db = svr["w4111f19"]
-doc_id, doc_rev = db.save({'type': 'Person', 'name': 'John Doe'})
